04b6770c35502c1ffc3aabeec7830c35c24ed0a9
Ku Jui <npukujui11@gmail.com>
2023-10-31 18:42:00 +0800

    特征叠加+注意力机制+InceptionResNetv2模型 训练代码
    Train，Features fusion，Attention，InceptionResNetv2

--- train/train_merge.py
+++ train/train_merge.py
@@ -1,86 +1,113 @@
 import os
-import cv2
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
-from tensorflow.keras.layers import Input, Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Input, Conv2D, GlobalAveragePooling2D, Dense, Multiply, Flatten, Softmax, Dropout
 from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
+from tensorflow.keras.applications import InceptionResNetV2
+from sklearn.utils.class_weight import compute_class_weight
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from sklearn.utils import shuffle
 
-# 数据路径
-data_dirs = {
-    'original': 'D:\\program\\potholes-detection\\dataset\\alldata_filled',
-    'edge': 'D:\\program\\potholes-detection\\dataset\\alldata_filled_edge',
-    'hog': 'D:\\program\\potholes-detection\\dataset\\alldata_filled_hog',
-    'lbp': 'D:\program\\potholes-detection\\dataset\\alldata_filled_lbp'
-}
+# 数据加载和预处理
+def load_data(data_dir, feature_dirs):
+    images = []
+    labels = []
+    for filename in os.listdir(data_dir):
+        if filename.startswith("pothole"):
+            label = [1, 0]
+        elif filename.startswith("normal"):
+            label = [0, 1]
+        else:
+            continue
 
-# 参数
-img_width, img_height = 640, 640
-batch_size = 32
-epochs = 50
-num_classes = 2
+        # 加载原始图像
+        img_path = os.path.join(data_dir, filename)
+        img = load_img(img_path, color_mode="grayscale", target_size=(640, 640))
+        img = img_to_array(img)
 
+        # 加载特征图
+        features = [img]
+        for feature_dir in feature_dirs:
+            feature_path = os.path.join(feature_dir, filename)
+            feature_img = load_img(feature_path, color_mode="grayscale", target_size=(640, 640))
+            feature_img = img_to_array(feature_img)
+            features.append(feature_img)
 
-# 加载数据并融合特征
-def load_and_merge_data(data_dirs):
-    X = []
-    y = []
-    for image_name in os.listdir(data_dirs['original']):
-        original_img = cv2.imread(os.path.join(data_dirs['original'], image_name), cv2.IMREAD_GRAYSCALE)
-        edge_img = cv2.imread(os.path.join(data_dirs['edge'], image_name), cv2.IMREAD_GRAYSCALE)
-        hog_img = cv2.imread(os.path.join(data_dirs['hog'], image_name), cv2.IMREAD_GRAYSCALE)
-        lbp_img = cv2.imread(os.path.join(data_dirs['lbp'], image_name), cv2.IMREAD_GRAYSCALE)
+        # 合并通道
+        merged = np.concatenate(features, axis=-1)
+        images.append(merged)
+        labels.append(label)
 
-        merged_img = np.dstack([original_img, edge_img, hog_img, lbp_img])
-        X.append(merged_img)
+    return np.array(images), np.array(labels)
 
-        label = 1 if 'pothole' in image_name else 0
-        y.append(label)
+data_dir = "D:\\program\\potholes-detection\\dataset\\alldata"
+feature_dirs = ["D:\\program\\potholes-detection\\dataset\\alldata_filled_edge",
+                "D:\\program\\potholes-detection\\dataset\\alldata_filled_hog",
+                "D:\\program\\potholes-detection\\dataset\\alldata_filled_lbp"]
 
-    return np.array(X), np.array(y)
+X, y = load_data(data_dir, feature_dirs)
+X, y = shuffle(X, y, random_state=42)  # 打乱数据
 
+# 划分训练和验证数据
+split = int(0.9 * len(X))
+X_train, X_val = X[:split], X[split:]
+y_train, y_val = y[:split], y[split:]
 
-X, y = load_and_merge_data(data_dirs)
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, stratify=y)
+# 注意力模块
+def attention_module(input_tensor, channels=4):
+    # 使用一个小型的卷积网络计算注意力权重
+    attention_probs = Conv2D(channels, kernel_size=(1, 1), activation='softmax', padding='same',
+                             name='attention_probs')(input_tensor)
 
-# 数据增强
-datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
-datagen.fit(X_train)
+    # 为边缘图（第二个通道）分配更高的初始权重
+    initial_bias = tf.constant_initializer([-2., 2., -2., -2.])  # 偏向于第二个通道，即边缘图
+    attention_probs_bias = Conv2D(channels, kernel_size=(1, 1), kernel_initializer='zeros',
+                                  bias_initializer=initial_bias, activation='softmax', padding='same',
+                                  name='attention_probs_bias')(input_tensor)
 
-# 构建模型
-input_tensor = Input(shape=(img_width, img_height, 4))
-base_model = InceptionResNetV2(weights='imagenet', include_top=False, input_tensor=input_tensor)
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-predictions = Dense(num_classes, activation='softmax')(x)
-model = Model(inputs=base_model.input, outputs=predictions)
+    # 将注意力权重与输入张量相乘
+    attention_mul = Multiply(name='attention_multiply')([input_tensor, attention_probs_bias])
 
-# 编译模型
+    return attention_mul
+
+# 模型定义
+def create_attention_model(input_shape):
+    input_tensor = Input(shape=input_shape)
+    attention_output = attention_module(input_tensor)
+
+    base_model = InceptionResNetV2(include_top=False, weights=None, input_tensor=attention_output)
+    base_features = base_model.output
+
+    gap = GlobalAveragePooling2D()(base_features)
+    fc1 = Dense(128, activation='relu')(gap)
+    dropout = Dropout(0.5)(fc1)  # 添加Dropout层
+    output = Dense(2, activation='softmax')(dropout)
+
+    model = Model(inputs=input_tensor, outputs=output)
+    return model
+
+input_shape = (640, 640, 4)
+model = create_attention_model(input_shape)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# 类权重
-class_weights = {
-    0: 1.0,  # normal
-    1: (266 / 35)  # potholes
-}
+# 使用数据增强和类权重处理数据不平衡问题
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.utils.class_weight import compute_class_weight
+
+datagen = ImageDataGenerator(rotation_range=20,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             horizontal_flip=True)
+datagen.fit(X_train)
+
+# 计算类权重
+y_integers = np.argmax(y_train, axis=1)
+class_weights = compute_class_weight('balanced', classes=np.unique(y_integers), y=y_integers)
+class_weights = dict(enumerate(class_weights))
 
 # 训练模型
-model.fit(datagen.flow(X_train, y_train, batch_size=batch_size),
-          validation_data=(X_val, y_val),
-          steps_per_epoch=len(X_train) // batch_size,
-          epochs=epochs,
-          class_weight=class_weights)
+model.fit(datagen.flow(X_train, y_train, batch_size=16), validation_data=(X_val, y_val), epochs=40, class_weight=class_weights)
 
 # 保存模型
 model.save('inception_resnet_v2_fused_features.h5')
