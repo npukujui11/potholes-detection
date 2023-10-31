29b42c99fdb5550684aa8d705d41c71bbf7aec21
Ku Jui <npukujui11@gmail.com>
2023-10-31 11:38:12 +0800

    HED edge detection
    HED 边缘图生成模型

--- edgedetect/edge_detection_hed.py
+++ edgedetect/edge_detection_hed.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 
+
 def hed_edge_detection(image_path, model_path, config_path):
     # 加载预训练的HED模型
     net = cv2.dnn.readNetFromCaffe(config_path, model_path)
@@ -21,10 +22,17 @@ def hed_edge_detection(image_path, model_path, config_path):
 
     return hed
 
-# 使用函数
-image_path = 'D:\\program\\potholes-detection\\dataset\\potholes1.jpg'  # 替换为您的图像路径
-model_path = 'D:\\program\\potholes-detection\\edgedetect\\hed_pretrained_bsds.caffemodel'  # 替换为您下载的模型权重路径
-config_path = 'D:\\program\\potholes-detection\\edgedetect\\deploy.prototxt'  # 替换为您下载的配置文件路径
+# 文件夹路径
+input_folder = 'D:\\program\\potholes-detection\\dataset\\alldata_filled'  # 图像文件夹路径
+output_folder = 'D:\\program\\potholes-detection\\dataset\\alldata_filled_edge'  # 输出边缘图的文件夹路径
+os.makedirs(output_folder, exist_ok=True)
 
-edge_image = hed_edge_detection(image_path, model_path, config_path)
-cv2.imwrite('edge_output.jpg', edge_image)
+model_path = 'D:\\program\\potholes-detection\\edgedetect\\hed_pretrained_bsds.caffemodel'  # 替换为您下载的模型权重路径
+config_path = 'D:\\program\\potholes-detection\\edgedetect\\hed_deploy.prototxt'  # 替换为您下载的配置文件路径
+
+# 遍历文件夹中的所有图片
+for image_name in os.listdir(input_folder):
+    image_path = os.path.join(input_folder, image_name)
+    edge_image = hed_edge_detection(image_path, model_path, config_path)
+    output_path = os.path.join(output_folder, image_name)
+    cv2.imwrite(output_path, edge_image)
